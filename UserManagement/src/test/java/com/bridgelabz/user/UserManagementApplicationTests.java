5f4ead18f8cfaa60fa01123fecdd64400bbabfed
kishlayswapnil <kishlayswapnil@gmail.com>
2020-06-20 11:59:58 +0530

    Added: Basic Project Structure has been added.

---
+++ UserManagement/src/test/java/com/bridgelabz/user/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
