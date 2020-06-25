7404471e63a63e7363870fc488e0dd098eb0194e
kishlayswapnil <kishlayswapnil@gmail.com>
2020-06-25 11:34:06 +0530

    Added: Token Exception has been added.

---
+++ UserManagement/src/main/java/com/bridgelabz/user/exception/TokenException.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.user.exception;
+
+public class TokenException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+    int errorCode;
+
+    public TokenException(String msg,int errorCode) {
+        super(msg);
+        this.errorCode=errorCode;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+}
