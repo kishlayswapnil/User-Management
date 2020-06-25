36a532282beaa17712d226b687580341e57a6653
kishlayswapnil <kishlayswapnil@gmail.com>
2020-06-25 11:37:42 +0530

    Added: Response model has been added.

---
+++ UserManagement/src/main/java/com/bridgelabz/user/model/Response.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.user.model;
+
+public class Response {
+
+    //Variable
+    private int statusCode;
+    private String statusMessage;
+
+    //Parameterised Constructor.
+    public Response(int statusCode, String message) {
+        super();
+        this.statusCode=statusCode;
+        this.statusMessage=message;
+    }
+
+    //Empty Constructor with super.
+    public Response() {
+        super();
+    }
+
+    //Getters and Setters
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
+
+    public void setStatusMessage(String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
+}
